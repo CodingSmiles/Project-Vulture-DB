63b1b74c46d49f7fbe236080f0646417dcbf86f4
CodingSmiles <95686935+CodingSmiles@users.noreply.github.com>
2021-12-13 20:13:19 +0530

    Added Some Code
    This code will only write data into the people.db file. The people.db file is not the only one that can be used. We will use people.db during the testing phase. Reading data doesn't work yet

--- main.py
+++ main.py
@@ -1,11 +1,60 @@
+from sqlite3.dbapi2 import connect
 from tkinter import *
-
-master = Tk()
-master.title("Project Vulture Test")
-master.geometry("360x305")
-# I use camelcase to code. I prefer it but I do know that Python's Official Formatting is PEP-8.
-# Currently the Label is packed into the screen but it could be changed
-testLabel = Label(master, text="Testing", font="Helvetica, 18").pack()
+import sqlite3
 
 
-master.mainloop()
+main = Tk()
+reader = Tk()
+
+def sendData():
+    titleContent = titleBox.get()
+    location = placeBox.get()
+    yearStr = yearBox.get()
+    yearInt = int(yearStr)
+    cursor.execute("INSERT INTO people (title, place, year) VALUES(?, ?, ?)", [titleContent, location, yearInt])
+    cursor.execute("SELECT * FROM people")
+    sendButton.config(state=DISABLED)
+
+def readData() :
+    global data
+    global list1
+    global listLength
+    list1 = []
+    listLength = len(list1)
+    data = cursor.execute('''SELECT * FROM people''')
+    for row in data:
+        list1.append(row)
+        listLength = len(list1)
+    dataLabel = Label(reader, text=list1)
+
+def closeConn() :
+    connection.commit()
+    connection.close()
+    exit()
+
+
+main.title("SQL Test")
+reader.title("View")
+connection = sqlite3.connect('people.db')
+cursor = connection.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS people
+             (title TEXT, place TEXT, year INT)''')
+titleBox = Entry(main, width=30, borderwidth=10)
+titleBox.grid(row=0, column=0)
+placeBox = Entry(main, width=30, borderwidth=10)
+placeBox.grid(row=1, column=0)
+yearBox = Entry(main, width=30, borderwidth=10)
+yearBox.grid(row=2, column=0)
+sendButton = Button(main, text="Enter Data", padx=10, pady=10, bg="green", command=sendData)
+closeButton = Button(main, text="Save And Close", padx=10, pady=10, bg="red", command=closeConn)
+sendButton.grid(row=3, column=0)
+sendButton.grid(row=4, column=0)
+titleLabel = Label(main, text="Title").grid(row=0, column=1)
+placeLabel = Label(main, text="Place").grid(row=1, column=1)
+yearLabel = Label(main, text="Year").grid(row=2, column=1)
+readButton = Button(reader, text="Read", padx=10, pady=10, bg="green", command=readData)
+readButton.grid(row=0, column=0)
+expLabel = Label(reader, text="Press to Read All Data").grid(row=0, column=1)
+
+main.mainloop()
+reader.mainloop()
