f09bae3e92459e0e36c8639b2ec2af0cb9199859
CodingSmiles <95686935+CodingSmiles@users.noreply.github.com>
2021-12-24 14:19:42 +0530

    Major Update to sw.py (See Description)
    Added and removed many lines of code. Also added better error prevention.

--- sw.py
+++ sw.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python # Allowing it to run
 from sqlite3.dbapi2 import connect
 from tkinter import *
 import sqlite3
 
 main = Tk()
-main.resizable(False, False)
-
+main.minsize(410, 310)
+readerBox = Frame(main, bg="light blue", height=100)
 
 def clearTable() :
     cursor.execute('DELETE FROM cars;',);
@@ -26,7 +26,7 @@ def sendData():
 def readData() :
     global frame1
     frame1 = Frame(main)
-    frame1.grid(row=7, column=1)
+    frame1.grid(row=5, column=1)
     global data
     global dataLabel
     global labelList
@@ -36,7 +36,7 @@ def readData() :
         dataLabel = Label(frame1, text=row)
         dataLabel.pack()
         labelList.append(dataLabel)
-    clearButton.config(state=ACTIVE)
+    clearButton.config(state=ACTIVE, bg="red", fg="black")
 
 
 def closeConn() :
@@ -48,8 +48,8 @@ def closeConn() :
 def clearData() :
     for dataLabel in labelList:
         dataLabel.destroy()
+    clearButton.config(state=DISABLED, bg="grey", fg="grey")
     frame1.destroy()
-    clearButton.config(state=DISABLED, bg="red")
 
 
 main.title("Vulture DB")
@@ -59,22 +59,22 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS cars
              (company TEXT, model TEXT, year INT)''')
 companyBox = Entry(main, width=30, borderwidth=10)
 companyBox.grid(row=0, column=1)
-
 modelBox = Entry(main, width=30, borderwidth=10)
 modelBox.grid(row=1, column=1)
 yearBox = Entry(main, width=30, borderwidth=10)
 yearBox.grid(row=2, column=1)
-sendButton = Button(main, text="Enter Data", padx=10, pady=10, bg="green", command=sendData).grid(row=4, column=0)
-closeButton = Button(main, text="Save And Close", padx=10, pady=10, bg="red", command=closeConn).grid(row=4, column=1)
+sendButton = Button(main, text="Enter Data", padx=10, pady=10, bg="green", command=sendData).grid(row=3, column=0)
+closeButton = Button(main, text="Save And Close", padx=10, pady=10, bg="red", command=closeConn).grid(row=3, column=1)
 companyLabel = Label(main, text="Company").grid(row=0, column=0)
 modelLabel = Label(main, text="Model").grid(row=1, column=0)
 yearLabel = Label(main, text="Year").grid(row=2, column=0)
-readButton = Button(main, text="Read", padx=10, pady=10, bg="green", command=readData).grid(row=6, column=0)
-clearButton = Button(main, text="Clear Printed Lines", padx=10, pady=10, bg="red", command=clearData)
-clearButton.grid(row=7, column=0)
-clearButton.config(state=DISABLED)
-clearTableButton = Button(main, text="Clear All Table Rows", padx=10, pady=10, bg="red", command=clearTable).grid(row=8, column=0)
-credit = Label(main, text="Made By Aadiraj Anil")
-credit.grid(row=10, column=0)
+readButton = Button(readerBox, text="Read", padx=10, pady=10, bg="green", command=readData).pack()
+clearButton = Button(readerBox, text="Clear Printed Lines", padx=10, pady=10, bg="grey", fg="grey", command=clearData, state=DISABLED)
+clearButton.pack()
+clearTableButton = Button(readerBox, text="Clear All Table Rows", padx=10, pady=10, bg="red", command=clearTable).pack()
+credit = Label(readerBox, text="Made By Aadiraj Anil", bg="light blue")
+credit.pack()
 
+
+readerBox.grid(row=5, column=0)
 main.mainloop()
